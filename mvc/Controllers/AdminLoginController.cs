d7217481708852176a6fcf617a439280dcbbc851
vamshi <MY PC@DESKTOP-RUA843G>
2026-02-19 15:57:01 +0530

    ADDED ADMIN MODULE WITH UI

---
+++ mvc/Controllers/AdminLoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvc.Models.UIMODELS;
+
+namespace mvc.Controllers
+{
+    public class AdminLoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult AdmLogin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AdmLogin(MCustomer model)
+        {
+            string Email = "v@g.com";
+            string password = "1234";
+            if (ModelState.IsValid) {
+                var res = model.Email == Email && model.password == password;
+                if (res == true)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+
+            }
+            return View();
+        }
+
+    }
+}
---
+++ mvc/Models/UIMODELS/MCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mvc.Models.UIMODELS
+{
+    public class MCustomer
+    {
+        //[Required(ErrorMessage ="Name is mandatory")]
+        [EmailAddress(ErrorMessage ="Invalid email format")]
+        [Required(ErrorMessage ="Email is mandatory")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage ="Password is mandatory")]
+        public string password { get; set; }
+
+    }
+}
